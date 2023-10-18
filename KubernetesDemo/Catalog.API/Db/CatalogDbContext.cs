52e12862b5f5a3d59b01ea741f54a1104243eca4
Akbar Ghane <a.ghane@faramin.local>
2023-10-18 13:43:01 +0330

    transfer from old git

---
+++ KubernetesDemo/Catalog.API/Db/CatalogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Db
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Catalog.API.Models.Product> Product { get; set; }
+    }
+}
